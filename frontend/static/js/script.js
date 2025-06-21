f04829801769ffc18ea33c8930ca5183b3e77af8
Natan França <natan.j.franca@gmail.com>
2025-06-21 20:27:07 -0300

    Página de login finalizada para desktop

---
+++ frontend/static/js/script.js
@@ -0,0 +1,13 @@
+var senhaInput = document.getElementById("senha");
+var olhoIcone = document.getElementById("toggleSenha");
+
+olhoIcone.onclick = function() {
+    if (senhaInput.type === "password") {
+        senhaInput.type = "text";
+        olhoIcone.src = "../static/img/interface/olho-aberto.png";
+    }
+    else {
+        senhaInput.type = "password";
+        olhoIcone.src = "../static/img/interface/olho-fechado.png";
+    }
+}
