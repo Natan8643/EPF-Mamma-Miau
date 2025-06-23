94b0a702dd276f26ccf7770c76b1a585336291b0
Natan França <natan.j.franca@gmail.com>
2025-06-22 22:20:43 -0300

    Pagina de cadastro finalizada e linkada com a pagina de login

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,7 +1,7 @@
 var senhaInput = document.getElementById("senha");
 var olhoIcone = document.getElementById("toggleSenha");
 
-olhoIcone.onclick = function() {
+olhoIcone.onclick = function () {
     if (senhaInput.type === "password") {
         senhaInput.type = "text";
         olhoIcone.src = "../static/img/interface/olho-aberto.png";
@@ -11,3 +11,10 @@ olhoIcone.onclick = function() {
         olhoIcone.src = "../static/img/interface/olho-fechado.png";
     }
 }
+
+document.querySelector('.form-cadastro').addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (this.checkValidity()) {
+        window.location.href = '../views/cadastro-confirm.html';
+    }
+})
