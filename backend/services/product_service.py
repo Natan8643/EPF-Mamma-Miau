56009e3ccc57a9d006fdb10defae4385d30787f8
Lucas Santos <kelytonlucas@gmail.com>
2025-06-30 21:02:48 -0300

    feat: consertando js

--- backend/services/product_service.py
+++ backend/services/product_service.py
@@ -1,20 +1,21 @@
 from sqlalchemy.orm import Session
 from models.product import Product
-
+from models.category import Category
 class ProductService:
     def __init__(self, db: Session):
         self.db = db
 
     def get_all_products(self):
         products = self.db.query(Product).all()
+                
         return [self.serialize_product(product) for product in products]
 
     def serialize_product(self, product):
         return {
-            "ProductID": product.ProductID,
-            "Name": product.Name,
-            "Category": product.Category,
-            "Price": str(product.Price),
+            "id": product.ProductID,
+            "nome": product.Name,
+            "categoria": product.Category,
+            "preco": str(product.Price),
         }
 
     def create_product(self, name:str, category:str, price:str):
