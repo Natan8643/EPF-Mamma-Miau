95cc5f4b6de0a19a92bc9653e65cd9d62aeec544
Natan França <natan.j.franca@gmail.com>
2025-07-03 00:29:18 -0300

    feat: adiciona melhores pratos com itegraçao do backend

---
+++ frontend/static/js/melhores-pratos.js
@@ -0,0 +1,36 @@
+async function buscarMelhoresPratos() {
+    try {
+        const resposta = await fetch('http://localhost:5000/best-dishes', {
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+      }
+    })
+        if (!resposta.ok) {
+            throw new Error("Erro ao buscar melhores pratos");            
+        }
+        const dados = await resposta.json();
+        
+        dados.best_dishes.forEach(dish => {
+            const pratoDiv = document.getElementById(`prato#${dish.posicao}`)
+            if (pratoDiv) {
+                const img = pratoDiv.querySelector('.img-melhores');
+                if (img) {
+                    img.src = dish.nome;
+                }
+                const nome = pratoDiv.querySelector('.texto-prato h4');
+                if (nome) nome.textContent = dish.nome;
+                const valor = pratoDiv.querySelector('.texto-prato p');
+                if (valor) valor.textContent = `R$ ${dish.total_vendido.toLocaleString('pt-BR', {minumFractionDigits: 2, maximunFractionDigits: 2})}`;
+                const contador = pratoDiv.querySelector('.contador');
+                if (contador) contador.textContent = dish.quantidade_vendida;
+                
+            }
+        });
+    } catch (error) {
+        console.error('Erro ao buscar melhores pratos do backend: ', error)
+        return [];
+    }
+}
+
+buscarMelhoresPratos();
+
